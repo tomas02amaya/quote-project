10ab52225187f23ef5baded1329037259b84bdd2
Tomas02Amaya <tomas02amaya@gmail.com>
2022-08-28 12:28:32 -0300

    Add updater with useEffect() / QuoteBoxr-component

--- src/component/QuoteBox.jsx
+++ src/component/QuoteBox.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import RandIn from '../assets/RandIn';
 import QuoteAndAuthor from "./QuoteAndAuthor";
 
+import colorList from '../json/colorList.json'
+
+
 const QuoteBox = () =>{
+    let [updater, setUpdater] = useState(true);
+
+    const changeStateUpdater = () =>{setUpdater(!updater)}    
+    
+    const randColor = () => {
+        let color = RandIn(colorList);
+        document.querySelector('body, .QuoteAndAuthor').style.background = color;
+        document.querySelector('blockquote').style.color = color;
+    };
+
+    useEffect(randColor,[updater]);
+
     return(
     <div className="quoteBox">
-
-        <button> &gt; </button>
+        <QuoteAndAuthor updater= {updater}/>
+        
+        <button onClick={changeStateUpdater}> &gt; </button>
     </div>
     );
 }
