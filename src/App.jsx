a32d38ea2950bacdc81326cb88372dbd0e9a03e2
Tomas02Amaya <tomas02amaya@gmail.com>
2022-08-28 12:31:39 -0300

    Clean App-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 import QuoteBox from './component/QuoteBox';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <QuoteBox />
